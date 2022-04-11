6ba9afc9466d57a8425cb1050fba38057b8902fd
Dreambig12 <sciencefi2@gmail.com>
2022-04-12 00:49:23 +0200

    Auth-Guard and User Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { MainLayoutComponent } from './components/layouts/main-layout/main-layout.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '' , component: MainLayoutComponent, children:[
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path:'home', component: HomeComponent},
     {path:'home/:username', component: HomeComponent},
+    {
+      path: 'user',
+      loadChildren: () => import('src/app/modules/user/user.module').then(m => m.UserModule)
+    },
   ]},
   {path:'login', component:LoginComponent},
    {path: 'register', component: RegisterComponent},
---
+++ src/app/modules/user/user.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
+
+const routes: Routes =[
+  {path:'', redirectTo: 'profile', pathMatch:'full'},
+  {path:'profile', component: UserProfileComponent, canActivate: [AuthGuard]},
+  // {path: 'EditProfile', component:EditUserProfileComponent, canActivate: [AuthGuard]}
+]
+
+
+@NgModule({
+  declarations: [
+    UserProfileComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class UserModule { }
