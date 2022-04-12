632a5eab55d512a469cbb1ce17a77f3773501611
Dreambig12 <sciencefi2@gmail.com>
2022-04-12 23:51:53 +0200

    products,Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { VmCardProduct } from 'src/app/models/view_models/VmCardProduct';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,15 @@ import { ActivatedRoute } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   username:string | null= '';
-  constructor(private activatedRoute:ActivatedRoute) { }
+  products: VmCardProduct[];
+  constructor(private activatedRoute:ActivatedRoute) {
+    this.products = [
+      {id: 1,name: 'test1',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
+      {id: 2,name: 'test2',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
+      {id: 3,name: 'test3',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
+      {id: 4,name: 'test4',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
+    ]
+  }
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((paramMap)=>{
