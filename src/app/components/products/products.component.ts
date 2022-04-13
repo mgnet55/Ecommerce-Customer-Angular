3a4b0be39c1ecf50f82335465c2c06408df0c8e7
Dreambig12 <sciencefi2@gmail.com>
2022-04-13 03:03:05 +0200

    products-service

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VmCardProduct } from 'src/app/models/view_models/VmCardProduct';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -8,17 +9,19 @@ import { VmCardProduct } from 'src/app/models/view_models/VmCardProduct';
 })
 export class ProductsComponent implements OnInit {
   searchBy: string | undefined;
-  products: VmCardProduct[];
-  constructor() {
-    this.products = [
-      {id: 1,name: 'test1',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
-      {id: 2,name: 'test2',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
-      {id: 3,name: 'test3',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
-      {id: 4,name: 'test4',price: 50,discount: 0,imageUrl: 'https://dummyimage.com/400x400/000/fff&text=user_avatar',category_name: 'cat1',available: 10},
-    ]
+  products: VmCardProduct[] = [];
+  constructor(private productsService:ProductsService) {
+
   }
 
   ngOnInit(): void {
+
+    this.productsService.getAllProducts().subscribe(ele=>{
+      this.products = ele.data.data
+
+      console.log(this.products);
+    })
+
 
     if (!this.searchBy) {
       //get all this.products
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private httpOptions = {};
+  constructor(private httpClient:HttpClient
+  ) {
+    this.httpOptions =  {headers: new HttpHeaders({
+      'Content-Type': 'application/json','Accept':' */*'
+      // ,'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+      })};
+
+
+   }
+
+   getAllProducts():Observable<any>{
+    return this.httpClient.get(`${environment.apiURL}/products`,this.httpOptions)
+   }
+
+}
